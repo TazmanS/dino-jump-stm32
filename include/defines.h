480f2001f4125735993e0e5fb01f839d0e3b4ca2
TazmanS <TazmanS123@gmail.com>
2025-08-07 20:16:15 +0300

    add interrupts

--- include/defines.h
+++ include/defines.h
@@ -7,6 +7,14 @@
 #define RCC_AHBENR (*(volatile uint32_t *)0x40021014)
 #define RCC_APB1ENR (*(volatile uint32_t *)0x4002101C)
 
+#define GPIOA_MODER (*(volatile uint32_t *)0x48000000) // GPIOA pin configuration register
+#define GPIOA_IDR (*(volatile uint32_t *)0x48000010)   // GPIOA input data register
+
+#define EXTI_IMR (*(volatile uint32_t *)0x40010400)   // EXTI interrupt mask register
+#define EXTI_FTSR (*(volatile uint32_t *)0x4001040C)  // EXTI falling trigger selection register
+#define EXTI_PR (*(volatile uint32_t *)0x40010414)    // EXTI pending register
+#define NVIC_ISER0 (*(volatile uint32_t *)0xE000E100) // NVIC interrupt set-enable register 0
+
 #define GPIOB_MODER (*(volatile uint32_t *)0x48000400)
 #define GPIOB_AFRH (*(volatile uint32_t *)0x48000424)
 #define GPIOB_OTYPER (*(volatile uint32_t *)0x48000404)
--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,9 @@ const char *menuItems[] = {
     "Settings",
     "About"};
 
+#define BUTTON_UP 0   // Button pin (PA0)
+#define BUTTON_DOWN 1 // Button pin (PA1)
+
 const uint8_t menuLength = sizeof(menuItems) / sizeof(menuItems[0]);
 volatile uint8_t menuIndex = 0;
 
@@ -23,15 +26,44 @@ void initLCDandI2C1()
 
 void EXTI0_IRQHandler() // up
 {
-  menuIndex = (menuIndex + 1) % menuLength;
+
+  if (EXTI_PR & (1 << BUTTON_UP))
+  {
+    EXTI_PR |= (1 << BUTTON_UP);
+    menuIndex = (menuIndex + 1) % menuLength;
+  }
 }
 
-void EXTI1_IRQHandler()
-{ // down
-  if (menuIndex == 0)
-    menuIndex = menuLength - 1;
-  else
-    menuIndex--;
+void EXTI1_IRQHandler() // down
+{
+  if (EXTI_PR & (1 << BUTTON_DOWN))
+  {
+    EXTI_PR |= (1 << BUTTON_DOWN);
+    if (menuIndex == 0)
+      menuIndex = menuLength - 1;
+    else
+      menuIndex--;
+  }
+}
+
+void interrupt_buttons_init()
+{
+  RCC_AHBENR |= (1 << 17);
+
+  GPIOA_MODER &= ~(0b11 << (BUTTON_UP * 2));
+  GPIOA_MODER &= ~(0b11 << (BUTTON_DOWN * 2));
+
+  // GPIOA_IDR |= (1 << BUTTON_UP);
+  // GPIOA_IDR |= (1 << BUTTON_DOWN);
+
+  EXTI_IMR |= (1 << BUTTON_UP);
+  EXTI_FTSR |= (1 << BUTTON_UP);
+
+  EXTI_IMR |= (1 << BUTTON_DOWN);
+  EXTI_FTSR |= (1 << BUTTON_DOWN);
+
+  NVIC_ISER0 |= (1 << 6);
+  NVIC_ISER0 |= (1 << 7);
 }
 
 void vTaskMenu()
@@ -47,11 +79,11 @@ void vTaskMenu()
 int main(void)
 {
   initLCDandI2C1();
-
+  interrupt_buttons_init();
   display_print(menuItems[menuIndex]);
 
-  // xTaskCreate(vTaskBlink, "blink", 48, NULL, 1, NULL);
-  // vTaskStartScheduler();
+  xTaskCreate((TaskFunction_t)vTaskMenu, "menu", 128, NULL, 1, NULL);
+  vTaskStartScheduler();
 
   while (1)
   {
